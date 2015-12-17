745bd0d824b84665117de686c96bb27ed3c7538c
Carl Xiong <xiongc05@gmail.com>
2015-12-17 15:56:01 +0800

    Display one hero
    * Use [(ngModel)] to establish two-way binding.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,24 @@
 import {Component} from 'angular2/core';
 
-@Component({
-	selector: 'my-app',
-	template: '<h1>Ng2 QuickStart</h1><p>But it is only a start.</p>'
-})
+// If we need a Hero that goes beyond simple properties, a Hero with logic and behavior, we must define a class. If we only need type checking, the interface is sufficient and lighter weight.
+interface Hero {
+    id: number;
+    name: string;
+}
 
-export class AppComponent { }
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+        </div>
+    `
+})
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero : Hero = { id: 1, name: 'Windstorm' };
+}
