1187aac7556630bfdc37adabc73443fd5e03f24d
Carl Xiong <xiongc05@gmail.com>
2015-12-17 15:23:20 +0800

    init code from quick start

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'my-app',
+	template: '<h1>Ng2 QuickStart</h1><p>But it is only a start.</p>'
+})
+
+export class AppComponent { }
