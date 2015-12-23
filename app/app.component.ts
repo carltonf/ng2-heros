661e547eca46c77e68d9b9c8c174b0f5d7a2b7b5
Carl Xiong <xiongc05@gmail.com>
2015-12-23 21:07:47 +0800

    Finish Part4: service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,11 @@
-import {Component, ViewEncapsulation} from 'angular2/core'
+import {Component, OnInit, ViewEncapsulation} from 'angular2/core'
 import {Hero} from './hero'
 // There is NO node style folder module resolution
 // ref: https://github.com/Microsoft/TypeScript/issues/5039
 import {HeroDetailComponent} from './hero-detail.component/index'
 import {HeroListComponent} from './hero-list.component/index'
 
-// TODO migrate to some data service
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
-];
+import {HeroService} from './hero.service/index'
 
 @Component({
   // Ref: http://blog.thoughtram.io/angular/2015/06/29/shadow-dom-strategies-in-angular2.html
@@ -34,11 +22,23 @@ var HEROES: Hero[] = [
         <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
   // NOTE: be explicit of using Array to avoid WebStorm warnings.
-  directives: [HeroDetailComponent, HeroListComponent]
+  directives: [HeroDetailComponent, HeroListComponent],
+  providers: [HeroService]
 })
-export class AppComponent {
-  public heroes = HEROES;
+export class AppComponent implements OnInit {
+  constructor(private _heroService: HeroService) {}
+  public heroes: Hero[];
   public title = 'Tour of Heroes';
   public selectedHero: Hero;
   public onSelect(hero: Hero) { this.selectedHero = hero; }
+
+  // NOTE: do NOT pass getHeroes as then callback as it would lose 'this'
+  // context
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  // Use life cycle hook to avoid complex logic in constructor
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
